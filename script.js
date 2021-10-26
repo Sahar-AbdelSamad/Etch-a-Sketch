b4b90b865a28a825baea952efd474266e10b0a0a
Sahar <sahar.abdelsamad@gmail.com>
2021-10-26 18:09:40 +0300

    added two button for colors

--- script.js
+++ script.js
@@ -1,34 +1,62 @@
+function changeColor(old) {
+    const items = document.querySelectorAll('.item');
+    items.forEach(item=>item.addEventListener("mouseover",()=> item.style.backgroundColor = old))
+}
+
+function choice(){
+    var choix= prompt("Choose your color","pink");
+    const items = document.querySelectorAll('.item');
+    items.forEach(item=>item.addEventListener("mouseover",()=>
+    {
+    changeColor(choix);
+    }))
+}
+
+function col(){
+    const items = document.querySelectorAll('.item');
+    items.forEach(item=>item.addEventListener("mouseover",()=>
+    {
+    var red= Math.floor(Math.random()*256);
+    var green= Math.floor(Math.random()*256);
+    var blue= Math.floor(Math.random()*256);
+    var c="RGB("+red+","+green+","+blue+")";
+    changeColor(c);
+    }))
+}
+
+function clear() {
+    var size = prompt('Choose the size your new Etch-a-Sketch (smaller then 100*100)', '10');
+    if(size>=100) var size = prompt('Please choose a size smaller then 100', '10');
+    if (size){
+    document.querySelectorAll('.item').forEach(item => div.removeChild(item));
+    root.style.setProperty('--a', size);
+    for (let x = 0; x < size * size; x++) {
+        var item = document.createElement('div');
+        item.className = "item";
+        div.appendChild(item); 
+        // const items = document.querySelectorAll('.item');
+        // items.forEach(itemX => itemX.addEventListener("mouseover", changeColor));
+    }}
+}
+
+const root = document.documentElement;
+
 const div = document.querySelector(".container")
 for (let x = 0; x < 16 * 16; x++) {
     var item = document.createElement('div');
     item.className = "item";
     div.appendChild(item);
 }
+var choix=choice;
 
+const items = document.querySelectorAll('.item');
+// items.forEach(itemX => itemX.addEventListener("mouseover", changeColor));
 
-function changeColor(e) {
-    this.style.background = "pink";
-}
+const buttonColor=document.querySelector('.choice')
+buttonColor.addEventListener("click",choice);
 
+const buttonRandom=document.querySelector('.random')
+buttonRandom.addEventListener("click",col);
 
 const button = document.querySelector("button");
 button.addEventListener("click", clear);
-function clear() {
-    document.querySelectorAll('.item').forEach((elem) => elem.remove());
-
-    var size = prompt('Choose the size your new Etch-a-Sketch (smaller then 100*100)', '10');
-if(size<100){
-    let root = document.documentElement;
-        root.style.setProperty('--a', size);
-        for (let x = 0; x < size * size; x++) {
-        var item = document.createElement('div');
-        item.className = "item";
-        div.appendChild(item);
-        const items = document.querySelectorAll('.item');
-        items.forEach(itemX => itemX.addEventListener("mouseover", changeColor));
-    }
-}
-else var size = prompt('Please choose a size smaller then 100', '10');
-}
-const items = document.querySelectorAll('.item');
-items.forEach(itemX => itemX.addEventListener("mouseover", changeColor));
